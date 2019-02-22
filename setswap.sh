0a406877e5e36a21a1a477e07baa581e6111bd55
Lacy Moore <lacy@aspendora.com>
2019-02-22 14:33:03 -0600

    Add files via upload

---
+++ setswap.sh
@@ -0,0 +1,9 @@
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+sudo sysctl vm.vfs_cache_pressure=50
+echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
